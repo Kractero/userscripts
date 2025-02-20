87cbcec7e5f521719329b3e26d488a0eafa1dc03
scramble <115837402+Kractero@users.noreply.github.com>
2025-02-20 09:36:14 -0700

    captcha qolers

---
+++ NSIssueRemoveTemplateNone.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         Issue Autocloser
+// @version      1.0
+// @match        https://*.nationstates.net/*page=enact_dilemma*
+// @exclude      https://*.nationstates.net/*page=show_dilemma*
+// @grant        window.close
+// @author       Kractero
+// ==/UserScript==
+
+(function () {
+    'use strict';
+    if (document.querySelector('.error')) {
+      document.addEventListener("keyup", function keyup (ev) {
+          if (ev.key === "Enter") {
+            window.location.href = `${window.location.href.replace('enact', 'show')}&userclick=${Date.now()}`
+          }
+          document.removeEventListener("keyup", keyup)
+      });
+      return
+    };
+    window.close();
+})();
