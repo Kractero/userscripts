bbc490735a606d95e29d27008fcbaba3d865d6b9
scramble <115837402+Kractero@users.noreply.github.com>
2025-02-27 14:06:44 -0700

    Update NSIssueCompactorRand.user.js

--- NSIssueCompactorRand.user.js
+++ NSIssueCompactorRand.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         NsIssueCompactorRandMod
-// @version      1.01
+// @version      1.02
 // @description  NsIssueCompactorRand but it text to speeches or plays audio when finding an iframe
 // @author       Kractero
 // @noframes
@@ -16,64 +16,58 @@
  * https://github.com/dithpri/RCES/blob/master/LICENSE.md for more details.
  */
 
-(function () {
-    let keyupListener = function (ev, chosenButtonNumber) {
-        if (ev.key !== "Enter" || ev.repeat || issuebtns[chosenButtonNumber].style.display === "none") {
-            ev.preventDefault();
-            return;
-        }
-        document.querySelectorAll("button.button.big.icon").forEach(function (el) {
-            el.style.display = "none";
-        });
-        issuebtns[chosenButtonNumber].click();
-        document.removeEventListener('keyup', keyupListener)
-    };
+;(function () {
+	let keyupListener = function (ev, chosenButtonNumber) {
+		if (ev.key !== 'Enter' || ev.repeat || issuebtns[chosenButtonNumber].style.display === 'none') {
+			ev.preventDefault()
+			return
+		}
+		document.querySelectorAll('button.button.big.icon').forEach(function (el) {
+			el.style.display = 'none'
+		})
+		issuebtns[chosenButtonNumber].click()
+		document.removeEventListener('keyup', keyupListener)
+	}
 
-    let keyupListener2 = function (ev) {
-        if (ev.key === "Enter") {
-            let newUrl = window.location.href.replace(/&?template-overall=none/, '');
-            window.location.href = `${newUrl}&userclick=${Date.now()}`;
-            document.removeEventListener('keyup', keyupListener2)
-        }
-    }
+	let keyupListener2 = function (ev) {
+		if (ev.key === 'Enter') {
+			let newUrl = window.location.href.replace(/&?template-overall=none/, '')
+			window.location.href = `${newUrl}?userclick=${Date.now()}`
+			document.removeEventListener('keyup', keyupListener2)
+		}
+	}
 
-    function checkForIframe(observer) {
-        const iframe = document.querySelector('iframe');
-        // let notifier = document.createElement('audio');
-        // notifier.volume = 0.3
-        // notifier.src = '';
-        if (iframe) {
-            // observer.disconnect();
-            // notifier.play();
-            document.removeEventListener('keyup', keyupListener)
-            if (window.location.href.includes('template-overall=none')) {
-                setTimeout(() => {
-                  document.addEventListener("keyup", (e) => keyupListener2(e, chosenButtonNumber));
-                }, 1000);
-            }
-            const speech = new SpeechSynthesisUtterance();
-            speech.text = `Recaptcha found`;
-            speech.voice = speechSynthesis.getVoices()[0];
-            window.speechSynthesis.speak(speech);
-        }
-    }
+	if (document.getElementsByClassName('recaptcha-box')[0]) {
+		// let notifier = document.createElement('audio')
+		// notifier.volume = 0.3
+		// notifier.src = ''
+		document.removeEventListener('keyup', keyupListener)
+		// notifier.play()
+		const speech = new SpeechSynthesisUtterance()
+		speech.text = `Recaptcha found`
+		speech.voice = speechSynthesis.getVoices()[0]
+		window.speechSynthesis.speak(speech)
+		if (window.location.href.includes('template-overall=none')) {
+			setTimeout(() => {
+				document.addEventListener('keyup', e => keyupListener2(e, chosenButtonNumber))
+			}, 1000)
+		}
+		return
+	}
 
-    const observer = new MutationObserver(() => checkForIframe(observer));
-    observer.observe(document.body, { childList: true, subtree: true });
+	const issuebtns = document.querySelectorAll('button.button.big.icon.approve')
+	if (issuebtns.length > 0) {
+		document.querySelector('p.dilemmadismissbox > button.big.icon.remove.danger').disabled = true
+		const chosenButtonNumber = 0
+		issuebtns[chosenButtonNumber].classList.add('rces-chosen')
 
-    const issuebtns = document.querySelectorAll("button.button.big.icon.approve");
-    if (issuebtns.length > 0) {
-        document.querySelector("p.dilemmadismissbox > button.big.icon.remove.danger").disabled = true;
-        const chosenButtonNumber = 0;
-        issuebtns[chosenButtonNumber].classList.add("rces-chosen");
-
-        if (!window.location.href.includes('template-overall=none')) {
-            // wait a second for interactivity for the iframe to fully load idk
-            setTimeout(() => {
-              document.addEventListener("keyup", (e) => keyupListener(e, chosenButtonNumber));
-            }, 1000);
-        } else {
-            document.addEventListener("keyup", (e) => keyupListener(e, chosenButtonNumber));
-        }
-    }
-})();
+		if (!window.location.href.includes('template-overall=none')) {
+			// wait a second for interactivity for the iframe to fully load idk
+			setTimeout(() => {
+				document.addEventListener('keyup', e => keyupListener(e, chosenButtonNumber))
+			}, 1000)
+		} else {
+			document.addEventListener('keyup', e => keyupListener(e, chosenButtonNumber))
+		}
+	}
+})()
