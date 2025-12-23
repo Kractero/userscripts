34f3770191f43d56edf4e904796b8344b4ed8ae9
scramble <115837402+Kractero@users.noreply.github.com>
2025-12-23 10:11:58 -0700

    Update scs.user.js

--- scs.user.js
+++ scs.user.js
@@ -2,7 +2,7 @@
 // @name        Simple Card Switcher
 // @match       https://*.nationstates.net/*generated_by=Hare*
 // @grant       window.close
-// @version     1.19
+// @version     1.2
 // @author      Kractero
 // @description Kill me
 // ==/UserScript==
