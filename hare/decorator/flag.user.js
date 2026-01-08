016281398b375902f096b5a38dadc59e8a5e74b3
scramble <115837402+Kractero@users.noreply.github.com>
2026-01-07 22:33:35 -0800

    Work with scs

--- hare/decorator/flag.user.js
+++ hare/decorator/flag.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Flag Uploader
-// @version      1.1
+// @version      1.2
 // @description  Flag blobbing
 // @author       Kractero
 // @match        https://*.nationstates.net/*page=upload_flag*
@@ -9,50 +9,50 @@
 
 ;(async function () {
     'use strict'
-  
+
     const flagUrl = ''
-  
+
     const openDatabase = () => {
       return new Promise((resolve, reject) => {
         const request = indexedDB.open('HareStorage', 2)
-  
+
         request.onupgradeneeded = e => {
           const db = e.target.result
           if (!db.objectStoreNames.contains('nsflags')) {
             db.createObjectStore('nsflags')
           }
         }
-  
+
         request.onsuccess = e => {
           resolve(e.target.result)
         }
-  
+
         request.onerror = e => {
           reject('IndexedDB error: ' + e.target.errorCode)
         }
       })
     }
-  
+
     const getFlagFromIndexedDB = (db, url) => {
       return new Promise((resolve, reject) => {
         const transaction = db.transaction(['nsflags'], 'readonly')
         const store = transaction.objectStore('nsflags')
         const request = store.get(url)
-  
+
         request.onsuccess = e => {
           resolve(e.target.result)
         }
-  
+
         request.onerror = e => {
           reject('Error retrieving flag from IndexedDB: ' + e.target.errorCode)
         }
       })
     }
-  
+
     const getFlag = async url => {
       const db = await openDatabase()
       let flagBlob = await getFlagFromIndexedDB(db, url)
-  
+
       if (!flagBlob) {
         console.log(`Fetching and storing flag: ${url}`)
         const response = await fetch(url)
@@ -63,32 +63,33 @@
       } else {
         console.log('Flag retrieved from IndexedDB')
       }
-  
+
       return flagBlob
     }
-  
+
     if (!flagUrl) {
       window.alert('Put a flag image link in line 13 please')
       return
     }
-  
+
     const flagBlob = await getFlag(flagUrl)
-  
+
     const file = new File([flagBlob], 'flag.png', { type: flagBlob.type })
-  
+
     const dataTransfer = new DataTransfer()
     dataTransfer.items.add(file)
-  
+
     const fileInput = document.querySelector("input[name='file']")
     fileInput.files = dataTransfer.files
-  
+
     const reader = new FileReader()
     reader.onload = function (e) {
       document.querySelector('#previewimage').src = e.target.result
       document.querySelector('#previewripple').src = ''
     }
     reader.readAsDataURL(flagBlob)
-  
-    document.querySelector('.primary').focus()
+
+    const loginBox = document.querySelector("#loginbox")
+    const isActive = loginBox.classList.contains("activeloginbox") || getComputedStyle(loginBox).display === "block"
+    if (!isActive) document.querySelector('.primary').focus()
   })()
-  
